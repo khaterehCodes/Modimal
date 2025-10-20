2e236193bbd2b17f1be5960d7844a7f04f3ce6e0
khatereh <khaterehnaseri444@gmail.com>
2025-10-20 10:02:27 +0330

    feat:create items array

--- src/components/ui/organisms/landingOrganisms/LandingOrganisms.jsx
+++ src/components/ui/organisms/landingOrganisms/LandingOrganisms.jsx
@@ -1,9 +1,11 @@
 import IntroLanding from "../../molecules/introLanding/IntroLanding";
+import BestSellers from "../bestSellers/BestSellers";
 
 function LandingOrganisms() {
     return (
-        <div className="w-full h-auto bg-pink-500 flex flex-col items-center md:gap-5">
+        <div className="w-full h-auto flex flex-col items-center md:gap-5">
             <IntroLanding />
+            <BestSellers />
         </div>
     );
 }
---
+++ src/core/constants/items/Items.js
@@ -0,0 +1,36 @@
+export const items = [
+    {
+        id: 1,
+        name: 'tailored stretch',
+        price: 180, description: 'turn it up pants',
+        img: '/assets/images/item1.png',
+        colors: [
+            { id: 1, color: '#0C0C0C' },
+            { id: 2, color: '#7DC3EB' },
+            { id: 3, color: '#748C70' },
+        ]
+    },
+    {
+        id: 2,
+        name: 'Technical Silk',
+        price: 120, description: 'Make A Splash',
+        img: '/assets/images/item2.jpg',
+        colors: [
+            { id: 1, color: '#909225' },
+            { id: 2, color: '#19418E' },
+            { id: 3, color: '#0C0C0C' },
+        ]
+    },
+    {
+        id: 3,
+        name: 'Cool Weave',
+        price: 210,
+        description: 'Anywhere Dress',
+        img: '/assets/images/item3.png',
+        colors: [
+            { id: 1, color: '#D0A5EA' },
+            { id: 2, color: '#909225' },
+            { id: 3, color: '#748C70' },
+        ]
+    },
+]
