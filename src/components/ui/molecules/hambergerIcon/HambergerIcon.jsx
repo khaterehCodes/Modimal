29571db2a8ea81ad0e05a466be271741f7f41aab
khatereh <khaterehnaseri444@gmail.com>
2025-10-22 10:31:55 +0330

    feat:add hamberger icon modal

--- src/components/ui/molecules/hambergerIcon/HambergerIcon.jsx
+++ src/components/ui/molecules/hambergerIcon/HambergerIcon.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import Button from "../../atom/customButton/Button";
 import Icon from "../../atom/icons/Icon";
+import HeaderMenuRes from "../headerMenuRes/HeaderMenuRes";
 
 function HambergerIcon() {
+    const [clickHamberger, setClickHamberger] = useState(false)
     return (
-        <div className="md:hidden w-[60px] h-[30px] flex items-center justify-between">
-            <Button>
-                <Icon name={'hamberger'}/>
-            </Button>
-            <Icon name={'searchIcon'}/>
-        </div>
+        <>
+            <div className="md:hidden w-[60px] h-[30px] flex items-center justify-between">
+                <Button onClick={() => setClickHamberger(!clickHamberger)}>
+                    {clickHamberger ? (<Icon name={'closeHamberger'} />) : (<Icon name={'hamberger'} />)}
+                </Button>
+                <Icon name={'searchIcon'} />
+            </div>
+            <HeaderMenuRes  clickHamberger={clickHamberger} setClickHamberger={setClickHamberger}/>
+        </>
     );
 }
 
