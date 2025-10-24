b3f841a557c7df95def85c5a34e4ec7ea7d4a12c
khatereh <khaterehnaseri444@gmail.com>
2025-10-24 19:35:40 +0330

    feat:create items array

--- src/core/constants/items/Items.js
+++ src/core/constants/items/Items.js
@@ -68,8 +68,8 @@ export const items = [
         category: 'pants',
         colors: [
             { id: 1, color: '#0C0C0C' },
-            { id: 1, color: '#CA2929' },
-            { id: 1, color: '#748C70' },
+            { id: 2, color: '#CA2929' },
+            { id: 3, color: '#748C70' },
         ]
     },
     {
@@ -81,8 +81,8 @@ export const items = [
         category: 'pants',
         colors: [
             { id: 1, color: '#0C0C0C' },
-            { id: 1, color: '#909225' },
-            { id: 1, color: '#748C70' },
+            { id: 2, color: '#909225' },
+            { id: 3, color: '#748C70' },
         ]
     },
     {
@@ -94,7 +94,7 @@ export const items = [
         category: 'pants',
         colors: [
             { id: 1, color: '#0C0C0C' },
-            { id: 1, color: '#748C70' },
+            { id: 2, color: '#748C70' },
         ]
     },
     {
@@ -106,7 +106,92 @@ export const items = [
         category: 'pants',
         colors: [
             { id: 1, color: '#0C0C0C' },
-            { id: 1, color: '#D0A5EA' },
+            { id: 2, color: '#D0A5EA' },
+        ]
+    },
+    {
+        id: 10,
+        name: 'Chill Wrap Top',
+        price: 160,
+        description: 'Turn It Up Top',
+        img: '/assets/images/item10.png',
+        category: 'top',
+        colors: [
+            { id: 1, color: '#909225' },
+        ]
+    },
+    {
+        id: 11,
+        name: 'Essential T-Shirt',
+        price: 95,
+        description: 'Turn It Up T-Shirt',
+        img: '/assets/images/item11.png',
+        category: 'shirt',
+        colors: [
+            { id: 1, color: '#0C0C0C' },
+            { id: 2, color: '#7DC3EB' },
+            { id: 3, color: '#748C70' },
+        ]
+    },
+    {
+        id: 12,
+        name: 'Shirt Dress',
+        price: 245,
+        description: 'Turn It Up Dress',
+        img: '/assets/images/item12.png',
+        category: 'dress',
+        colors: [
+            { id: 1, color: '#0C0C0C' },
+            { id: 2, color: '#7DC3EB' },
+            { id: 3, color: '#748C70' },
+        ]
+    },
+    {
+        id: 13,
+        name: 'Rule Zip Jacket',
+        price: 199,
+        description: 'Turn It Up Jacket',
+        img: '/assets/images/item13.png',
+        category: 'jacket',
+        colors: [
+            { id: 1, color: '#909225' },
+            { id: 2, color: '#CA6D29' },
+        ]
+    },
+    {
+        id: 14,
+        name: 'New Age Linen',
+        price: 180,
+        description: 'Turn It Up Pants',
+        img: '/assets/images/item14.jpg',
+        category: 'pants',
+        colors: [
+            { id: 1, color: '#0C0C0C' },
+            { id: 2, color: '#19418E' },
+            { id: 3, color: '#748C70' },
+        ]
+    },
+    {
+        id: 15,
+        name: 'Boss Pullover',
+        price: 280,
+        description: 'Turn It Up Pullover',
+        img: '/assets/images/item15.jpg',
+        category: 'pullover',
+        colors: [
+            { id: 1, color: '#0C0C0C' },
+            { id: 2, color: '#748C70' },
+        ]
+    },
+    {
+        id: 16,
+        name: 'crop it up pants',
+        price: 145,
+        description: 'Turn It Up Pants',
+        img: '/assets/images/item16.png',
+        category: 'pants',
+        colors: [
+            { id: 1, color: '#0C0C0C' },
         ]
     },
 ]
